aad97f1e9626386582c0b914e16263bffc768055
mmartinz6 <madmartinez@gmail.com>
2025-12-17 14:35:01 -0600

    chat entrenador-usuario

--- ruta-fitness/src/pages/ChatEntrenador.jsx
+++ ruta-fitness/src/pages/ChatEntrenador.jsx
@@ -73,7 +73,7 @@ const ChatCliente = () => {
           onClick={enviarMensaje}
           className="bg-indigo-600 text-white px-4 py-2 rounded-full"
         >
-          Enviar
+          Enviar Mensaje
         </button>
       </div>
     </div>
